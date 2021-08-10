63fda5f53b7490424fcd24e8093612145ed7110e
Artem <69114846+Artemcor@users.noreply.github.com>
2021-08-10 18:14:34 +0300

    fixViewController

--- project/project/ViewController.swift
+++ project/project/ViewController.swift
@@ -7,12 +7,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var googleButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            emailTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var passwordTextField: UITextField! {
+        didSet {
+            passwordTextField.delegate = self
+        }
+    }
+    
+    
+    var radius = 8.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        facebookButton.layer.cornerRadius = CGFloat(radius)
+        googleButton.layer.cornerRadius = CGFloat(radius)
+        loginButton.layer.cornerRadius = CGFloat(radius)
     }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        if email != "", password != "" {
+            if isValidEmail(email!) && isValidatePassword(password!) {
+                let alert = UIAlertController(title: "Congratulation", message: "You have signed successfully", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in }))
+                present(alert, animated: true, completion: {})
+            } else {
+                let alert = UIAlertController(title: "Oh no!!!", message: "You should write right password and Email", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in }))
+                present(alert, animated: true, completion: {})
+
+            }
+        } else {
+            let alert = UIAlertController(title: "Warning", message: "You should fill password and Email", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in }))
+            present(alert, animated: true, completion: {})
+
+        }
+    }
+    
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    func isValidatePassword(_ password: String) -> Bool {
+        if password.count > 5 {
+            return true
+        }
+        return false
+    }
+    
 
 
 }
